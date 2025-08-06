bc02bee1ed50f3d5c32798ea390cbe987d4cd7c4
Thapelo Mmakola <mthapelothapza@gmail.com>
2025-08-06 22:02:49 +0200

    initialise api project, install needed dependencies and create server

---
+++ README.md
@@ -0,0 +1,18 @@
+### Create a scalable and maintainable GraphQL API
+
+### Should allow customers to plan their travels
+
+consumers should be able to request the following:
+
+- weather forecast for selected city
+- ranking of activities based on the weather forecast
+
+use openmeto api for data
+
+STACK CHOICES
+
+use GraphQL for schema and querying 
+use axios for data fetching from api
+user apollo server
+use jest for unit test
+use 
---
+++ server.js
@@ -0,0 +1,18 @@
+
+const { ApolloServer } = require('apollo-server');
+
+// Function to start the Apollo Server
+async function startServer() {
+
+    // This function initializes the server 
+    const server = new ApolloServer();
+
+    // Start the server and listen on port 4000
+    server.listen({ port: 4000 }).then(({ url }) => {
+        // Log the URL where the server is running to the console
+        console.log(`🚀 Server ready at ${url}`);
+    });
+}
+
+// Invoke the function to start the server
+startServer();
